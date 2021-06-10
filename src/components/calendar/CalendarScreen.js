b7f24c1a7562446163726138e01caaf02782c7e5
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-10 17:41:24 -0300

    inicio del proyecto calendar

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavBar } from '../ui/NavBar'
+
+export const CalendarScreen = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand">
+                Sebastian
+            </span>
+            <button className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+                <span> Salir</span>
+            </button>
+        </div>
+    )
+}
