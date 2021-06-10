7127b08b642d081a0affb6a6310afae6546c089e
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-10 20:11:39 -0300

    big calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { NavBar } from '../ui/NavBar'
+import React from 'react';
+import { NavBar } from '../ui/NavBar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const localizer = momentLocalizer(moment);
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+}]
+
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen">
             <NavBar/>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
+import './style.css';
 
 ReactDOM.render(
   <CalendarApp/>,
