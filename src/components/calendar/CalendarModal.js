d04c2e12e4f8fb0f0ea654649e9294887da158e5
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-10 23:50:05 -0300

    formulario con validaciones

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
+import Swal from 'sweetalert2';
 
 const customStyles = {
     content: {
@@ -14,30 +16,145 @@ const customStyles = {
 };
 
 Modal.setAppElement('#root');
+const now = moment().minute(0).seconds(0).add(1, 'hours');
+const nowPlus1 = moment().clone().add(1, 'hours');
 
 
 export const CalendarModal = () => {
 
-    const [isOpen, setIsOpen] = useState(true);
+
+    const [dateStart, setDateStart] = useState(now.toDate());
+    const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
+    const [titleValid, setTitleValid] = useState(true);
+
+    const [formValues, setFormValues] = useState({
+        title: 'Evento',
+        notes: '',
+        start: now.toDate(),
+        end: nowPlus1.toDate()
+    });
+
+    const {title, notes, start, end} = formValues;
+
+    const handleInputChange = ({target}) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
 
     const closeModal = () => {
-        setIsOpen(false);
         console.log('cerrando...');
     }
-    
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e);
+        setFormValues({
+            ...formValues,
+            start: e
+        });
+    }
+
+    const handleEndDateChange = (e) => {
+        setDateEnd(e);
+        setFormValues({
+            ...formValues,
+            end: e
+        });
+    }
+
+    const handleSubmitForm = (e) =>{
+        e.preventDefault();
+        
+        const momentStart = moment(start);
+        const momentEnd = moment(end);
+
+        if(momentStart.isSameOrAfter(momentEnd)){
+            Swal.fire('Error', 'La fecha fin debe ser mayor a la de inicio', 'error');
+            return;
+        }
+
+        if(title.trim().length < 2){
+            return setTitleValid(false);
+        }
+
+        setTitleValid(true);
+        closeModal();
+
+    }
+
     return (
         <Modal
-            isOpen={isOpen}
+            isOpen={true}
             onRequestClose={closeModal}
             style={customStyles}
             closeTimeoutMS={200}
             className="modal"
             overlayClassName="modal-fondo"
         >
-            <h1>Hola mundo</h1>
-            <hr/>
-            <span>Hola de nuevo...</span>
+            <h1> Nuevo evento </h1>
+            <hr />
+            <form 
+                className="container"
+                onSubmit={handleSubmitForm}>
+
+                <div className="form-group">
+                    <label>Fecha y hora inicio</label>
+                    <DateTimePicker
+                        onChange={handleStartDateChange}
+                        value={dateStart}
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Fecha y hora fin</label>
+                    <DateTimePicker
+                        onChange={handleEndDateChange}
+                        value={dateEnd}
+                        className="form-control"
+                        minDate={dateStart}
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className={`form-control ${!titleValid && "is-invalid"}`}
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={title}
+                        onChange={handleInputChange}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={notes}
+                        onChange={handleInputChange}
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
         </Modal>
-            
+
     )
 }
