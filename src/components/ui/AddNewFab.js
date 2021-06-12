b72fe4cbec57799a9b430b57c9c82c09a1215112
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-11 23:21:19 -0300

    abrir desde boton con fecha de hoy

--- src/components/ui/AddNewFab.js
+++ src/components/ui/AddNewFab.js
@@ -1,13 +1,17 @@
+import moment from 'moment';
 import React from 'react'
 import { useDispatch } from 'react-redux';
-import { uiOpenModal } from '../../actions/ui';
+import { uiOpenModalParams } from '../../actions/ui';
 
 export const AddNewFab = () => {
 
     const dispatch = useDispatch();
 
-    const handleClick = () =>{
-        dispatch(uiOpenModal());
+    const handleClick = () => {
+        const now = moment().minute(0).seconds(0).add(1, 'hours').toDate();
+        const nowPlus1 = moment().clone().add(1, 'hour').toDate();
+
+        dispatch(uiOpenModalParams(now, nowPlus1));
     }
 
     return (
